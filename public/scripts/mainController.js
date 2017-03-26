b1fc7474494bd211d405d3390f1b7f935473c2e9
einarzb <einarzb@gmail.com>
2017-03-26 22:39:45 +0300

    dropdown works. now working on adding the req.user in the nav bar, like hello john

--- public/scripts/mainController.js
+++ public/scripts/mainController.js
@@ -1,9 +1,13 @@
 app.controller('mainController', function($scope) {
 $scope.signUp = true;
 $scope.loginScreen = false;
+$scope.user;
+
 $scope.signUpBtn = function(){
   $scope.signUp = false;
   $scope.loginScreen = true;
+  $scope.user = req.user;
+
 
 }
 
