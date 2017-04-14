634e901711d343dddbb3ca75c4e1578372cd52f3
einarzb <einarzb@gmail.com>
2017-04-14 17:36:25 +0300

    fixed login page

--- public/scripts/controllers/mainController.js
+++ public/scripts/controllers/mainController.js
@@ -1,7 +1,9 @@
 app.controller('mainController', function($scope, $http, $location) {
 
 
-
+$scope.getUserName = function(){
+  alert("im submit");
+}
 $scope.signUpBtn = function(){
   console.log("im singup button");
 };
