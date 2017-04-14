364440cb3277147a39a57759b2eae8649e0a620d
einarzb <einarzb@gmail.com>
2017-04-14 16:38:52 +0300

    organized all pages by the routing system, defined routing in app.js and created new templates also added href base for locationprovider

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,24 @@
 var app = angular.module('loginApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise('home');
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/templates/partial-home.html',
+      controller: 'mainController'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: '/templates/login.html',
+      controller: 'mainController'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: '/templates/register.html',
+      controller: 'mainController'
+    })
+
+});
