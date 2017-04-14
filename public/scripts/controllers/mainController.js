87aef420a85885410a9b2eb87cf9ded3e88c7cc2
einarzb <einarzb@gmail.com>
2017-04-14 16:55:39 +0300

    added function of getting user name after login to navctrl instead of main controler, did some styling

--- public/scripts/controllers/mainController.js
+++ public/scripts/controllers/mainController.js
@@ -1,33 +1,10 @@
 app.controller('mainController', function($scope, $http, $location) {
-$scope.signUp = true;
-$scope.loginScreen = false;
 
-$scope.user = getParameterByName('username');
 
 
 $scope.signUpBtn = function(){
-  $scope.signUp = false;
-  $scope.loginScreen = true;
+  console.log("im singup button");
 };
 
-$scope.getUserName = function(){
-  return $http.get('/success').then(function(response){
-//  alert(response);
-  //  $scope.user = response.data;
-  });
-}
 
 });
-
-
-function getParameterByName(name, url) {
-    if (!url) {
-      url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
--- public/scripts/controllers/navController.js
+++ public/scripts/controllers/navController.js
@@ -1,3 +1,29 @@
-app.controller('NavCtrl', function() {
-  //todo - you may find it useful to use this controller for the navbar functionality
+app.controller('NavCtrl', function($scope, $http, $location) {
+
+//hide log in and register buttons 
+
+
+  //getting user name (yohai help)
+  $scope.user = getParameterByName('username');
+
+  $scope.getUserName = function(){
+    return $http.get('/success').then(function(response){
+   alert(response);
+     $scope.user = response.data;
+    });
+  };
+
+  //yohai function for getting user name
+  function getParameterByName(name, url) {
+      if (!url) {
+        url = window.location.href;
+      }
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
 });
