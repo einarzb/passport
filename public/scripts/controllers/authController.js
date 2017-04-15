56502c1e5fc4b976f4411e0bdfe2ca0a43362dfa
einarzb <einarzb@gmail.com>
2017-04-15 20:35:03 +0300

    added regex pattern for password

--- public/scripts/controllers/authController.js
+++ public/scripts/controllers/authController.js
@@ -1,5 +1,5 @@
 app.controller('authController', function($scope, $http, $location) {
-
+  
     $scope.register = function(newUser){
       console.log("im in authController");
       if (newUser.username === newUser.passwordConfirm) {
