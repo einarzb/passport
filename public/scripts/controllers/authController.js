17b188234b0b34300fdf85d7bb8b649cc7ea47ed
einarzb <einarzb@gmail.com>
2017-04-15 20:56:22 +0300

    after submit state.go to success

--- public/scripts/controllers/authController.js
+++ public/scripts/controllers/authController.js
@@ -1,11 +1,10 @@
-app.controller('authController', function($scope, $http, $location) {
-  
+app.controller('authController', function($scope, $http, $location, $state) {
+
     $scope.register = function(newUser){
       console.log("im in authController");
-      if (newUser.username === newUser.passwordConfirm) {
-          console.log("password are the same");
-      } else {
-        $scope.missmatch = false;
-      };
+      console.log(newUser);
+      //clear input fields
+      $scope.username = "";
+      $state.go('success');
     }
 });
