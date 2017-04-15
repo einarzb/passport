ced6f1881abe44e288579cd5aee4012839c136a6
einarzb <einarzb@gmail.com>
2017-04-15 22:42:06 +0300

    added function submit to login in ctrler and service

--- public/scripts/controllers/authController.js
+++ public/scripts/controllers/authController.js
@@ -6,9 +6,21 @@ app.controller('authController', function($scope, $http, $location, $state, auth
       //using promise we can wait for a successful registration before we re-reroute the user.
       .then(function(){
         $state.go('success');
-      //display error  
+      //display error
       }, function(err){
         alert(err.data.message);
+      });
+    };
+
+    $scope.login = function(){
+      console.log("im in authController login button", $scope.user);
+      authFactory.login($scope.user)
+      //using promise we can wait for a successful registration before we re-reroute the user.
+      .then(function(){
+        $state.go('success');
+      //display error
+      }, function(err){
+        alert(err.data); //different error is thrown by passport-authenticate
       });
     };
 
--- public/scripts/services/authFactory.js
+++ public/scripts/services/authFactory.js
@@ -2,8 +2,14 @@ app.factory('authFactory', function($http) {
     var auth = {};
 
     auth.register = function(user) {
-      console.log("im in factory");
+      console.log("im in factory register req");
       return $http.post('/users/register', user);
     };
+
+    auth.login = function(user){
+      console.log("im in factory login req");
+      return $http.post('/users/login', user);
+    };
+
     return auth;
   });
