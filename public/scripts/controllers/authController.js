587687cf32eba42254acaabd6047002b3e9c4414
einarzb <einarzb@gmail.com>
2017-04-15 18:40:58 +0300

    design bootstrap

--- public/scripts/controllers/authController.js
+++ public/scripts/controllers/authController.js
@@ -1,7 +1,12 @@
 app.controller('authController', function($scope, $http, $location) {
-
     $scope.register = function(newUser){
       console.log("im in authController");
       console.log(newUser);
+      console.log(newUser.username);
+      if (newUser.username === newUser.passwordConfirm) {
+          console.log("password are the same");
+      } else {
+        console.log("password dont match");
+      };
     }
 });
