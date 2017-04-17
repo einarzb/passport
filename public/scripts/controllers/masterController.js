540d7c80188995afb0d887b5ea55b19e6416fb4e
einarzb <einarzb@gmail.com>
2017-04-17 21:30:25 +0300

    test

--- public/scripts/controllers/masterController.js
+++ public/scripts/controllers/masterController.js
@@ -1,3 +1,4 @@
 app.controller('masterController', function($scope, authFactory) {
-    $scope.currentUser = authFactory.currentUser
+    $scope.currentUser = authFactory.currentUser;
+    console.log($scope.currentUser);
 });
